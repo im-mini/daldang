c5428c0e5b44d00f3c5534e41b15e8fcf20de377
im-mini <47658549+im-mini@users.noreply.github.com>
2026-02-04 13:18:39 +0900

    Create data.js

---
+++ data.js
@@ -0,0 +1,176 @@
+// 카페 제품 데이터
+const products = [
+  // 스타벅스
+  {
+    id: 1,
+    brand: '스타벅스',
+    name: '아메리카노',
+    category: '커피',
+    sugar: 0,
+    size: 'Tall (355ml)'
+  },
+  {
+    id: 2,
+    brand: '스타벅스',
+    name: '카페 라떼',
+    category: '커피',
+    sugar: 17,
+    size: 'Tall (355ml)'
+  },
+  {
+    id: 3,
+    brand: '스타벅스',
+    name: '자바칩 프라푸치노',
+    category: '프라푸치노',
+    sugar: 48,
+    size: 'Tall (355ml)'
+  },
+  {
+    id: 4,
+    brand: '스타벅스',
+    name: '카라멜 마키아또',
+    category: '커피',
+    sugar: 25,
+    size: 'Tall (355ml)'
+  },
+  {
+    id: 5,
+    brand: '스타벅스',
+    name: '자몽 허니 블랙티',
+    category: '티',
+    sugar: 30,
+    size: 'Tall (355ml)'
+  },
+  {
+    id: 6,
+    brand: '스타벅스',
+    name: '아이스크림 블렌딩 콜드브루',
+    category: '블렌디드',
+    sugar: 53,
+    size: 'Tall (355ml)'
+  },
+  {
+    id: 7,
+    brand: '스타벅스',
+    name: '말차 라떼',
+    category: '티',
+    sugar: 32,
+    size: 'Tall (355ml)'
+  },
+  {
+    id: 8,
+    brand: '스타벅스',
+    name: '바닐라 라떼',
+    category: '커피',
+    sugar: 35,
+    size: 'Tall (355ml)'
+  },
+  
+  // 투썸플레이스
+  {
+    id: 9,
+    brand: '투썸플레이스',
+    name: '아메리카노',
+    category: '커피',
+    sugar: 0,
+    size: '기본 (355ml)'
+  },
+  {
+    id: 10,
+    brand: '투썸플레이스',
+    name: '카페 라떼',
+    category: '커피',
+    sugar: 15,
+    size: '기본 (355ml)'
+  },
+  {
+    id: 11,
+    brand: '투썸플레이스',
+    name: '20곡 오틀리 라떼',
+    category: '커피',
+    sugar: 24,
+    size: '기본 (315ml)'
+  },
+  {
+    id: 12,
+    brand: '투썸플레이스',
+    name: '초코 케이크',
+    category: '디저트',
+    sugar: 38,
+    size: '1조각'
+  },
+  {
+    id: 13,
+    brand: '투썸플레이스',
+    name: '딸기 케이크',
+    category: '디저트',
+    sugar: 42,
+    size: '1조각'
+  },
+  {
+    id: 14,
+    brand: '투썸플레이스',
+    name: '티라미수',
+    category: '디저트',
+    sugar: 35,
+    size: '1조각'
+  },
+  
+  // 이디야
+  {
+    id: 15,
+    brand: '이디야',
+    name: '아메리카노',
+    category: '커피',
+    sugar: 0,
+    size: 'Large (532ml)'
+  },
+  {
+    id: 16,
+    brand: '이디야',
+    name: '카페 라떼',
+    category: '커피',
+    sugar: 24,
+    size: 'Large (532ml)'
+  },
+  {
+    id: 17,
+    brand: '이디야',
+    name: '멜팅 피스타치오',
+    category: '음료',
+    sugar: 49,
+    size: 'Large (532ml)'
+  },
+  {
+    id: 18,
+    brand: '이디야',
+    name: '헤이즐넛 젤라또 카페모카',
+    category: '음료',
+    sugar: 71,
+    size: 'Extra (680ml)'
+  },
+  {
+    id: 19,
+    brand: '이디야',
+    name: '너티 초콜릿',
+    category: '음료',
+    sugar: 58,
+    size: 'Large (532ml)'
+  },
+  {
+    id: 20,
+    brand: '이디야',
+    name: '아샷추 복숭아',
+    category: '음료',
+    sugar: 57,
+    size: 'Large (532ml)'
+  },
+  {
+    id: 21,
+    brand: '이디야',
+    name: '제로슈가 달달커피',
+    category: '커피',
+    sugar: 0,
+    size: 'Large (532ml)'
+  }
+];
